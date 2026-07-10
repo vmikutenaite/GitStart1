6b79f2c73b4783404e84bafc651f666cf7d0c01f
vmikutenaite <v.mikutenaite@ba.lt>
2026-07-10 14:08:37 +0300

    removed one step
    removed one step

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -13,7 +13,4 @@ test('get started link', async ({ page }) => {
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
