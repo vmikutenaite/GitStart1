54336b2027e8f2d5a026d78e1b8fed13be322df6
vmikutenaite <v.mikutenaite@ba.lt>
2026-07-10 14:26:03 +0300

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -14,4 +14,5 @@ test('get started link', async ({ page }) => {
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
   //add comment
+  //dar vienas komentaras
 });
