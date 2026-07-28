85046a1e5a3f42944df12dda83a0263b738d3c7e
vmikutenaite <v.mikutenaite@ba.lt>
2026-07-28 14:39:13 +0300

    arrays (#3)

---
+++ tests/arrays.js
@@ -0,0 +1,43 @@
+// let fruits = ["coconut", "apple","pear"];
+// console.log(fruits);
+// console.log(fruits[1])
+
+// // ismesti
+// // fruits.pop();
+// // ideti pabaigoj
+// // fruits.push("banana");
+// // ideti pradziok
+// // fruits.unshift("strawberry");
+
+// // skaicius array element
+// console.log(fruits.length)
+// // indeksas elemento
+// console.log(fruits.indexOf("apple"))
+
+// // iprastas loop array
+// for(let i = 0; i < fruits.length; i++){
+// console.log(fruits[i]);
+// }
+// // paprastas loop array
+// for (fruit of fruits){
+// console.log(fruit);
+// }
+
+
+
+// // sort pagal abc ir atv
+// fruits.sort();
+// fruits.sort().reverse();
+
+let maistas = ["tortas", "duona", "grybai"];
+console.log(maistas);
+
+maistas.push("sviestas");
+console.log(maistas);
+
+maistas.unshift("kiaušiniai");
+console.log(maistas);
+
+for(let i = 0; i < maistas.length; i++){
+console.log(maistas[i]);
+}
